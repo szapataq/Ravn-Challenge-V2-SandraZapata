139d2fb7deb07a39f616b478314065c4ae434bac
szapataq <szapata013@gmail.com>
2020-09-22 14:17:08 -0500

    adding route for 2 views

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
-
+import DetailCharacter from './views/DetailCharacter';
+import Home from './views/Characters';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar/>
-    </div>
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/characters/:id' exact component={DetailCharacter}/>
+      </Switch>
+    </Router>
   );
 }
-
 export default App;
+
