6f873c89066a02329abfe2d9c42b9e3467de55d9
szapataq <szapata013@gmail.com>
2020-09-22 08:04:34 -0500

    creation of Navbar components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/Navbar.scss';
+
+const Navbar = () => {
+    return (
+        <nav className="navigation">
+            <p>Ravn Star Wars Registry</p>
+        </nav>
+    )
+}
+
+export default Navbar;
+
