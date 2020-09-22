36545773d05701b8fd2ca043bc9dafcfeb5c8421
szapataq <szapata013@gmail.com>
2020-09-22 14:18:13 -0500

    adding apollo provider for the client

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/scss/bootstrap.scss';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './graphql/client';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
