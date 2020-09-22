38e2e8a02034f2a51bb01d42737200025076015a
szapataq <szapata013@gmail.com>
2020-09-22 18:40:37 -0500

    Redirecting each cell of character

--- src/views/Characters.jsx
+++ src/views/Characters.jsx
@@ -3,6 +3,8 @@ import { useQuery} from '@apollo/client';
 import ALL_PEOPLE from '../graphql/query.js';
 import Loading from '../components/Loading';
 import Error from '../components/Error';
+import arrowLeft from "../assets/icons/arrow-right.svg";
+import { Link } from "react-router-dom";
 
 const Characters = () => {
     const {data, loading, error}= useQuery(ALL_PEOPLE);
@@ -16,14 +18,20 @@ const Characters = () => {
     });
 
     return (
-        <div className="containter-character-cell">
+        <div className="containter-characters">
             {
                 characters.map(item => {
                   return(
-                    <div key={item.id} className="character-cell">
-                      <h2 className="Name-character">{item.name}</h2>
-                      <p>from {item.homeworld}</p>
-                    </div>
+                    <Link key={item.id} className="link-detail" to={`/characters/${item.id}`}>
+                      <div className="character-cell">
+                        <div>
+                          <h2>{item.name}</h2>
+                          <p>from {item.homeworld}</p>
+                        </div>
+                        <img src={arrowLeft} alt="arrow-left"></img>
+                      </div>
+                    </Link>
+
                   )                  
                 })
             }
