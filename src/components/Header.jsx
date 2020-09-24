ebbbbbae1487f89567f76e346615417c0acb7f82
szapataq <szapata013@gmail.com>
2020-09-23 19:29:00 -0500

    renaming component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/Navbar.scss";
+
+const Header = () => {
+  return (
+    <nav className="navigation">
+      <h2>Ravn Star Wars Registry</h2>
+    </nav>
+  );
+};
+
+export default Header;
